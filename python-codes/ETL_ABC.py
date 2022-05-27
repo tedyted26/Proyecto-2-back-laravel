7e0899b463b3d0944ec9497aad9ac33487b289ab
tedyted26 <tedyted26@hotmail.com>
2022-05-27 13:59:19 +0200

    try catch scraper abc

--- python-codes/ETL_ABC.py
+++ python-codes/ETL_ABC.py
@@ -20,28 +20,33 @@ def getABCNews(categoria: String, paginas = 1):
         
         for li in resultados.findAll("li"):
             link = li.find("a")["href"]
-
-            html_noticia = rq.urlopen(link, context=ssl.SSLContext()).read()
-            soupTmp = BeautifulSoup(html_noticia, 'html.parser')
-
-            encabezado = soupTmp.find(class_="encabezado-articulo")
-            titulo = encabezado.find(class_="titular").text
-            subtitulo = encabezado.find(class_="subtitulo").text
-
-            cuerpo = soupTmp.find(class_="cuerpo-texto")
-            fecha = cuerpo.find(class_="fecha").find("time")["datetime"]
-            texto = " ".join([x.text for x in cuerpo.findAll("p")])
+            html_noticia = ""
             try:
-                tags = [x.text for x in cuerpo.find(class_="modulo temas").findAll("li")]
-            except:
-                tags = []
+                html_noticia = rq.urlopen(link, context=ssl.SSLContext()).read()
+            except: 
+                pass
 
-            bloqueCOM = cuerpo.find(class_="comentarios")
-            listaCOM = cuerpo.findAll(class_="gig-comment-body")
-            #Los comentarios no funcionan porque se carga dinamicamente
-            #comentarios = [x.text for x in cuerpo.findAll(class_="gig-comment-body")]
-            
-            noticia = Noticia(titulo, subtitulo, fecha, link, categoria, "ABC", tags, texto)
-            listaNoticias.append(noticia)
-            #print(titulo,link, "\n----\n")
+            if html_noticia != "":
+                soupTmp = BeautifulSoup(html_noticia, 'html.parser')
+
+                encabezado = soupTmp.find(class_="encabezado-articulo")
+                titulo = encabezado.find(class_="titular").text
+                subtitulo = encabezado.find(class_="subtitulo").text
+
+                cuerpo = soupTmp.find(class_="cuerpo-texto")
+                fecha = cuerpo.find(class_="fecha").find("time")["datetime"]
+                texto = " ".join([x.text for x in cuerpo.findAll("p")])
+                try:
+                    tags = [x.text for x in cuerpo.find(class_="modulo temas").findAll("li")]
+                except:
+                    tags = []
+
+                bloqueCOM = cuerpo.find(class_="comentarios")
+                listaCOM = cuerpo.findAll(class_="gig-comment-body")
+                #Los comentarios no funcionan porque se carga dinamicamente
+                #comentarios = [x.text for x in cuerpo.findAll(class_="gig-comment-body")]
+                
+                noticia = Noticia(titulo, subtitulo, fecha, link, categoria, "ABC", tags, texto)
+                listaNoticias.append(noticia)
+                #print(titulo,link, "\n----\n") 
     return listaNoticias
