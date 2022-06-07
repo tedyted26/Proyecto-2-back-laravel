f471f0a26cfa7365fe23b93868f34b4378174628
tedyted26 <tedyted26@hotmail.com>
2022-06-07 22:50:36 +0200

    MIGRATE

---
+++ database/migrations/2022_06_07_193443_create_config_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->id();
+            $table->string("contact_mail");
+            $table->string("contact_num");
+            $table->integer("is_abc");
+            $table->integer("is_20min");
+            $table->integer("is_elpais");
+            $table->integer("is_lasexta");
+            $table->integer("is_lavang");
+            $table->integer("is_larazon");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+};
